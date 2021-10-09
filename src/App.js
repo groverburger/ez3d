4f19687265606d4d589e20701feac79364302972
Antonio Ibarrola <ntonio.ibarrola@gmail.com>
2021-10-08 18:14:59 -0700

    Demo Cube Model and Grid

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import './styles.css'
 
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const ref = useRef()
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => {
-    ref.current.rotation.x = ref.current.rotation.y += 0.01
-  })
+function Box() {
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={active ? 1.5 : 1}
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    <mesh>
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
     </mesh>
   )
 }
 
 export default function App() {
   return (
-    <Canvas>
+    <Canvas camera={{ position: [3, 3, 3] }}>
+      <OrbitControls />
       <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <spotLight position={[0, 5, 10]} angle={0.3}/>
+      <gridHelper position={[0, -0.51, 0]} args={[100, 100, "#787878", "#989898"]}/>
+      <fog attach="fog" args={["#d9d9db", 10, 20]} />
+      <Box />
     </Canvas>
   )
 }
+
+// import React, { useRef, useState } from 'react'
+// import { Canvas, useFrame } from '@react-three/fiber'
+// import { OrbitControls } from 'drei'
+
+// function Box(props) {
+//   // This reference will give us direct access to the mesh
+//   const ref = useRef()
+//   // Set up state for the hovered and active state
+//   const [hovered, setHover] = useState(false)
+//   const [active, setActive] = useState(false)
+//   // Rotate mesh every frame, this is outside of React without overhead
+//   useFrame(() => {
+//     ref.current.rotation.x = ref.current.rotation.y += 0.01
+//   })
+//   return (
+//     <mesh
+//       {...props}
+//       ref={ref}
+//       scale={active ? 1.5 : 1}
+//       onClick={(e) => setActive(!active)}
+//       onPointerOver={(e) => setHover(true)}
+//       onPointerOut={(e) => setHover(false)}>
+//       <boxGeometry args={[1, 1, 1]} />
+//       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+//     </mesh>
+//   )
+// }
+
+// export default function App() {
+//   return (
+//     <Canvas>
+//       <ambientLight intensity={0.5} />
+//       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+//       <pointLight position={[-10, -10, -10]} />
+//       <Box position={[-1.2, 0, 0]} />
+//       <Box position={[1.2, 0, 0]} />
+//     </Canvas>
+//   )
+// }
