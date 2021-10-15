e53399cb0699aa74597768d5169ab2fccbfadc55
Ruiyang Liu <rliu60@ucsc.edu>
2021-10-14 22:15:53 -0700

    edited by Ruiyang, create cube by click

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
+import React, { useRef, useState } from 'react'
+import { Canvas} from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import './styles.css'
 
 /**
  * Basic implementation to creating a Box element.
  */
-function Box() {
-  return (
-    <mesh>
+// function Box() {
+//   return (
+//     <mesh position={[1, 2, 3]}>
+//       <boxBufferGeometry attach="geometry" />
+//       <meshLambertMaterial attach="material" color="hotpink" />
+//     </mesh>
+//   )
+// }
+
+// edited by Ruiyang
+// create a box on origin
+// reference: https://codesandbox.io/s/r3f-basic-demo-forked-6q6ww?file=/src/App.js:600-604
+function Box(props) {
+  const mesh = useRef()
+   return (
+    <mesh {...props} ref={mesh}>
       <boxBufferGeometry attach="geometry" />
       <meshLambertMaterial attach="material" color="hotpink" />
     </mesh>
   )
 }
+
 
 /**
  * Create a Canvas to render elements into the scene.
@@ -27,25 +41,70 @@ function Box() {
  * - args           [Color, Minimum Distance, Maximum Distance]
  * Box              Includes the Box element into the scene
  */
+
+// edited by Ruiyang, click button, create new box
 export default function App() {
+  const [boxes, setBoxes] = useState([])
+
   return (
+    // create canvas and button in one section.
     <>
       <div>
-        <button>
+        <button onClick={() => generateNewBlock()}>
           hihi
         </button>
       </div>
-      <Canvas camera={{ position: [3, 3, 3] }}>
+      <Canvas 
+        onClick={() => Box()}
+        camera={{ position: [3, 3, 3] }}>
+      
         <OrbitControls />
         <ambientLight intensity={0.5} />
         <spotLight position={[0, 5, 10]} angle={0.3}/>
+        {boxes.map((props) => (
+        <Box {...props} />
+        ))}
         <gridHelper position={[0, -0.51, 0]} args={[100, 100, "#787878", "#989898"]}/>
         <fog attach="fog" args={["#d9d9db", 10, 20]} />
-        <Box />
+        
       </Canvas>
     </>
   )
+
+  // edited by Ruiyang
+  function generateNewBlock() {
+    const total = boxes.length
+    let newBoxes = boxes
+    newBoxes.push({position: [0, total * 2 , 0]})
+    console.log(total)
+    setBoxes([...newBoxes])  
+  }
+
 }
+
+// export default function App() {
+//   return (
+//     <>
+//       <div>
+//         <button onClick={test}>
+//           hihi
+//         </button>
+//       </div>
+//       <Canvas 
+//         onClick={() => Box()}
+//         camera={{ position: [3, 3, 3] }}>
+      
+//         <OrbitControls />
+//         <ambientLight intensity={0.5} />
+//         <spotLight position={[0, 5, 10]} angle={0.3}/>
+//         <gridHelper position={[0, -0.51, 0]} args={[100, 100, "#787878", "#989898"]}/>
+//         <fog attach="fog" args={["#d9d9db", 10, 20]} />
+        
+//       </Canvas>
+//     </>
+//   )
+
+// }
 
 // import React, { useRef, useState } from 'react'
 // import { Canvas, useFrame } from '@react-three/fiber'
