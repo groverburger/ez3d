9e7860a6e2b8f95b923a3c74c53555499cfe422d
Gabi Aizenman <lucky3gabi@gmail.com>
2021-11-10 09:15:52 -0800

    Selector default value fix
    Now the color selector starts on the correct color

--- src/components/colorSelector.js
+++ src/components/colorSelector.js
@@ -1,7 +1,11 @@
+import { useColor } from './context';
+
 export default function ColorSelect({ onInput }) {
+  const { colorChange } = useColor();
     return (
         <input
           type="color"
+          value={colorChange}
           onInput={onInput}
         />
     );
