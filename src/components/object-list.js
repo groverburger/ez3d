9c23a9a3a308c1f95b6fee551c15c059d50104d6
cachtayl <54186417+cachtayl@users.noreply.github.com>
2021-11-09 23:40:27 -0800

    Single Selection from the ObjList
    Still has a bug but the functionality is there

--- src/components/object-list.js
+++ src/components/object-list.js
@@ -1,5 +1,8 @@
 import { useGroup, useModel } from './context';
+import { useTransform } from './context';
 import '../styles/object-list.css';
+
+
 
 // ObjectList populates the top pane of the outliner with a list of Meshes in the scene. The saved state value is
 // an object of arrays that holds all the positions for any generated shape. Each array corresponds to their
@@ -10,21 +13,48 @@ export default function ObjectList() {
 
   var total = modelData.length;
   var objList = [];
+  var spheresTot = 0;
+  var cubesTot = 0;
+  var cylindersTot = 0;
   for (let i = 0; i < total; i++) {
-    objList.push({ name: `Object ${i}` });
+    switch (modelData[i].type){
+      case 'cube':
+        cubesTot++;
+        objList.push({ name: `Cube ${cubesTot}`,
+                          index: `${i}`});
+        break;
+      case 'sphere':
+        spheresTot++;
+        objList.push({ name: `Sphere ${spheresTot}`,
+                          index: `${i}`});
+        break;
+      case 'cylinder':
+        cylindersTot++;
+        objList.push({ name: `Cylinder ${cylindersTot}`,
+                          index: `${i}`});
+        break;
+      default:
+        objList.push({ name: `Object ${i}`,
+                          index: i});
+        break;
+    }
+
   }
 
-  console.log(groupList);
-
+   console.log(groupList);
+  const { setTargetToTransform } = useTransform();
   return (
     <>
       <div className='object-list-top'>
         <label className='object-list-title'>Object List</label>
-        {objList.map((mesh) => (
-          <div key='{mesh}' className='object-list-items'>
-            <label> {mesh.name} </label>
-          </div>
-        ))}
+          {objList.map((mesh) => (
+            <div key='{mesh}'>
+              <button
+                className='btn-light object-list-items'
+                onClick={() => setTargetToTransform(groupList[mesh.index])}
+              > {mesh.name} </button>
+            </div>
+          ))}
       </div>
     </>
   );
